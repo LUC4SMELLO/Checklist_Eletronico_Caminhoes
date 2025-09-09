c4affb4426dcc1cf424f1ffdf1c84b150f97e0e3
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-09-09 16:27:58 -0300

    criando rota cadastro

---
+++ routes/cadastro.py
@@ -0,0 +1,7 @@
+from flask import Blueprint, render_template
+
+cadastro_bp = Blueprint("/cadastro", __name__)
+
+@cadastro_bp.route("/cadastro")
+def cadastro():
+    return render_template("cadastro.html")
