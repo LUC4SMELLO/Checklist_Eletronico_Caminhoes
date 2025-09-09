aa05b10e289fda8084986c0e04b1cddc39b0f639
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-09-09 15:43:19 -0300

    adicionando rota /login

--- app.py
+++ app.py
@@ -3,11 +3,13 @@ from flask import Flask
 from routes.homepage import homepage_bp
 from routes.checklist_html import checklist_bp
 from routes.checklist_post import checklist_post_bp
+from routes.login import login_bp
 
 app = Flask(__name__)
 app.register_blueprint(homepage_bp)
 app.register_blueprint(checklist_bp)
 app.register_blueprint(checklist_post_bp)
+app.register_blueprint(login_bp)
 
 
 if __name__ == "__main__":
