6a743ca4f848b5c03088daee8835ae493a2723c0
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-09-09 16:29:46 -0300

    adicionando rota /cadastro

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from routes.homepage import homepage_bp
 from routes.checklist_html import checklist_bp
 from routes.checklist_post import checklist_post_bp
 from routes.login import login_bp
+from routes.cadastro import cadastro_bp
 
 app = Flask(__name__)
 app.register_blueprint(homepage_bp)
 app.register_blueprint(checklist_bp)
 app.register_blueprint(checklist_post_bp)
 app.register_blueprint(login_bp)
+app.register_blueprint(cadastro_bp)
 
 
 if __name__ == "__main__":
