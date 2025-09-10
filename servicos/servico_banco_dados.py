4ee451289a98615141b4cd5f1d91fc1c392cbfd8
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-09-10 10:48:33 -0300

    incluindo banco dados usuarios

--- servicos/servico_banco_dados.py
+++ servicos/servico_banco_dados.py
@@ -1,5 +1,7 @@
 from database.banco_dados_checklist import criar_banco_dados_checklist
+from database.banco_dados_usuarios import criar_banco_dados_usuarios
 
 def inicializar_bancos_dados():
     
     criar_banco_dados_checklist()
+    criar_banco_dados_usuarios()
