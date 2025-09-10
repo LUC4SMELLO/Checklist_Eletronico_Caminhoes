3b260e4e2516dd1ce85bcb8a5027727ed1785eb4
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-09-10 10:36:30 -0300

    criando banco dados usuarios

---
+++ database/banco_dados_usuarios.py
@@ -0,0 +1,51 @@
+import sqlite3
+
+def conectar_banco_dados_usuarios():
+    return sqlite3.connect("TabelaUsuarios.db")
+
+def criar_banco_dados_usuarios():
+
+    conexao = conectar_banco_dados_usuarios()
+    cursor = conexao.cursor()
+
+    cursor.execute(
+    """
+    CREATE TABLE IF NOT EXISTS TabelaUsuarios (
+    nome_completo VARCHAR(100),
+    senha VARCHAR(50)
+    )
+    """
+    )
+
+    conexao.commit()
+    conexao.close()
+
+def inserir_usuario(nome_completo, senha):
+
+    conexao = conectar_banco_dados_usuarios()
+    cursor = conexao.cursor()
+
+    cursor.execute(
+    """
+    INSERT INTO TabelaUsuarios (nome_completo, senha)
+    VALUES (?, ?)
+    """, (nome_completo, senha)
+    )
+
+    conexao.commit()
+    conexao.close()
+
+def excluir_usuario(nome_completo, senha):
+
+    conexao = conectar_banco_dados_usuarios()
+    cursor = conexao.cursor()
+
+    cursor.execute(
+    """
+    DELETE FROM TabelaUsuarios
+    WHERE nome_completo = ? AND senha = ?
+    """, (nome_completo, senha)
+    )
+
+    conexao.commit()
+    conexao.close()
