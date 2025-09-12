ae3df468e12714c62120be2d8b4012f29427214f
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-09-12 17:12:28 -0300

    cadastro de usuário funcionando

--- routes/cadastro.py
+++ routes/cadastro.py
@@ -1,7 +1,31 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, request, redirect, url_for, render_template
+
+from database.banco_dados_usuarios import (
+    buscar_usuario,
+    inserir_usuario
+)
 
 cadastro_bp = Blueprint("cadastro", __name__)
 
-@cadastro_bp.route("/cadastro")
+@cadastro_bp.route("/cadastro", methods=["GET", "POST"])
 def cadastro():
+
+    if request.method == "POST":
+
+        nome_completo = request.form.get("nome_completo")
+        senha = request.form.get("senha")
+
+        resultado = buscar_usuario(nome_completo)
+
+        if resultado:
+            erro = "Usuário Já Encontrado."
+
+            return render_template("cadastro.html", erro=erro)
+        
+        else:
+
+            inserir_usuario(nome_completo, senha)
+
+            return redirect(url_for("checklist.checklist", nome_usuario=nome_completo))
+
     return render_template("cadastro.html")
