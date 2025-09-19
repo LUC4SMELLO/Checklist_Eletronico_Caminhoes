f2e2bfbcebec072396cdb4a62cae090325e92392
Lucas Mello <lucaspereirasilvamello@gmail.com>
2025-09-19 15:14:26 -0300

    validador cadastro

--- routes/cadastro.py
+++ routes/cadastro.py
@@ -1,9 +1,10 @@
 from flask import Blueprint, request, flash, redirect, url_for, render_template
 
 from database.banco_dados_usuarios import (
-    buscar_usuario,
     inserir_usuario
 )
+
+from backend.validadores.validar_cadastro import validar_cadastro
 
 cadastro_bp = Blueprint("cadastro", __name__)
 
@@ -15,16 +16,14 @@ def cadastro():
         nome_completo = request.form.get("nome_completo")
         senha = request.form.get("senha")
 
-        resultado = buscar_usuario(nome_completo, senha)
-
-        if resultado:
-            flash("Usuário Já Encontrado.", "erro")
+        valido, mensagem = validar_cadastro(nome_completo, senha)
+        if not valido:
+            flash(mensagem, "erro")
             return render_template("cadastro.html")
         
         else:
             inserir_usuario(nome_completo, senha)
-            
-            flash("Cadastro Bem Sucedido!", "sucesso")
+    
             return redirect(url_for("checklist.checklist", nome_usuario=nome_completo))
 
     return render_template("cadastro.html")
